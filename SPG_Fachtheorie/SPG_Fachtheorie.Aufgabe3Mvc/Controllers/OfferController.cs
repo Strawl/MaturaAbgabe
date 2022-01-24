ea5034a3ae24410054982490523928b28f5f07d8
Dominik Gyarmati <gya18170@spengergasse.at>
2022-01-24 18:41:36 +0100

    Adding Details and Add.cshtml

--- SPG_Fachtheorie/SPG_Fachtheorie.Aufgabe3Mvc/Controllers/OfferController.cs
+++ SPG_Fachtheorie/SPG_Fachtheorie.Aufgabe3Mvc/Controllers/OfferController.cs
@@ -25,9 +25,8 @@ namespace SPG_Fachtheorie.Aufgabe3Mvc.Controllers
         private Guid? IsCoach()
         {
             var coach = _appointmentContext.Students.ToList().Find(person => person.Username == _authService.Username);
-            if (coach != null && coach is Coach){
+            if (coach != null && coach is Coach)
                 return coach.Id;
-            }
             return null;
         }
 
@@ -36,11 +35,37 @@ namespace SPG_Fachtheorie.Aufgabe3Mvc.Controllers
         public IActionResult Index()
         {
             Guid? coachId = IsCoach();
+
             var context = _appointmentContext.Offers
                 .Where(offer => offer.TeacherId == coachId)
                 .Include(x => x.Subject)
                 .Include(x => x.Teacher);
             return View(context);
+        }
+
+        // /Offer/Details/{offerGuid}
+        [Authorize()]
+        public IActionResult Details(Guid? id)
+        {
+            Guid? coachId = IsCoach();
+            if (id == null || coachId == null)
+                return Redirect("/");
+
+            var appointment = _appointmentContext.Appointments
+                .Include(i => i.Student)
+                .Where(x => x.OfferId == id);
+
+            return View(appointment);
+        }
+
+        [Authorize()]
+        public IActionResult Add()
+        {
+            Guid? coachId = IsCoach();
+            if (coachId == null)
+                return Redirect("/");
+
+            return View();
         }
     }
 }
